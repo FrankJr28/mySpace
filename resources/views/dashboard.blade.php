284a3045539891d35421d870a3f84df77ffea89a
FrankJr28 <francisco.vasquezjr@alumnos.udg.mx>
2024-05-04 23:00:53 -0500

    drive try 1

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,22 @@
     <?php
         if(isset($_SESSION["access_token"])){
             echo "your access token is: ".$_SESSION["access_token"];
+            //$driveService = new Google\Service\Drive($client);
+            $client = app('MyGoogleClient');
+            //$retrievedToken = $client->getAccessToken();
+            //var_dump($retrievedToken);
+            //echo "<br/><br/>The retrieved token is: ".$retrievedToken;
+            $client->setAccessToken($_SESSION["access_token"]);
+            //$retrievedToken = $client->getAccessToken();
+            //echo "<br/><br/>The retrieved token is: ".$client->getAccessToken()["access_token"];
+            /************************************************************************************/
+            $driveService = new Google\Service\Drive($client);
+
+            $files = $driveService->files->listFiles(['q' => "'root' in parents"]);
+
+            foreach ($files->getFiles() as $file) {
+                echo $file->getName() . "<br/><br/>";
+            }
         }
         else{
             echo "no session set";
